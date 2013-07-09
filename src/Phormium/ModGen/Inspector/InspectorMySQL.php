1d9292b862f34bbf5204c5c3c1aaa427fcd0c0eb
Ivan Habunek <ivan.habunek@gmail.com>
2013-07-09 15:45:50 +0200

    Style fix

--- src/Phormium/ModGen/Inspector/InspectorMySQL.php
+++ src/Phormium/ModGen/Inspector/InspectorMySQL.php
@@ -78,7 +78,6 @@ class InspectorMySQL extends Inspector
             WHERE table_schema = :schema
               AND table_name = :table
               AND constraint_name = 'PRIMARY';
-
         ;";
         $args = compact('table', 'schema');
         $data = $conn->preparedQuery($query, $args);
