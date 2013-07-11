757ac3a797f7260ee3e878b5684a88da16211b82
Ivan Habunek <ivan.habunek@gmail.com>
2013-07-11 11:43:05 +0200

    Debug info to find out why travis fails.

--- tests/config/bootstrap.php
+++ tests/config/bootstrap.php
@@ -3,4 +3,5 @@
 $loader = require __DIR__ . '/../../vendor/autoload.php';
 $loader->add('Phormium\\ModGen\\Tests', __DIR__ . '/../');
 
+var_dump(getcwd(), PHORMIUM_CONFIG_FILE);
 Phormium\DB::configure(PHORMIUM_CONFIG_FILE);
